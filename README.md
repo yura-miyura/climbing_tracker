7d6bd7249aa5f7002a0516d8f1d262d1d640b0f1
Yurii Artym <140413112+yura-miyura@users.noreply.github.com>
2026-04-04 09:33:46 +0100

    engine created

--- README.md
+++
@@ -1,2 +0,0 @@
-# climbing_trekker
-Full stack project, climbing trekker web app to keep track of your personal achievements and training 
---
+++ database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# lite locat database
+SQLALCHEMY_DATABASE_URL = "sqlite:///./bouldering.db"
+
+# created the sqlite engine and opened multithred on sqlite to allow python
+engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+        )
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
